3a7e2e199613f43ae46332e1d0ddd4d69f96d206
MishGun94 <kolczov9494@gmail.com>
2024-04-07 17:19:24 +0500

    Добавьте файлы проекта.

---
+++ Jet.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef JET_HPP
+#define JET_HPP
+#include <iostream>
+#include <string>
+
+class Super_jet
+{
+public:
+	
+
+private:
+	int health;
+
+};
+
+
+
+
+
+
+
+#endif JET_HPP
